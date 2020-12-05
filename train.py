13b8804694744b96565e7a1c7e00843ec679c2a5
Marius <mariux255@gmail.com>
2020-12-05 01:55:52 +0100

    Voting server run

--- train.py
+++ train.py
@@ -17,26 +17,27 @@ from datetime import datetime
 from torch.optim.lr_scheduler import StepLR
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 print(device)
-num_classes = 100
-dataset = MyCustomDataset("labels_100")
+num_classes = 2000
+dataset = MyCustomDataset("labels_2000")
 #dataset = MyCustomDataset(category='labels_100',json_file_path="/home/marius/Documents/Projects/WLASL_v0.3.json", frame_location="/home/marius/Documents/Projects/Processed_data")
 
 dataset_size = (len(dataset))
 
-val_size = int(np.floor(dataset_size * 0.2))
-train_size = int(dataset_size - val_size)
-trainset, validset = random_split(dataset, [train_size, val_size])
+val_size = int(np.floor(dataset_size * 0.1))
+test_size = int(np.floor(dataset_size * 0.1))
+train_size = int(dataset_size - val_size - test_size)
+trainset, validset, testset = random_split(dataset, [train_size, val_size, test_size])
 bs = 20
-num_classes = 100
-dataloader_train = DataLoader(trainset, batch_size=bs, shuffle=True, num_workers=2)
-dataloader_val = DataLoader(validset, batch_size=bs, shuffle=True, num_workers=2)
-
+num_classes = 2000
+dataloader_train = DataLoader(trainset, batch_size=bs, shuffle=True, num_workers=5)
+dataloader_val = DataLoader(validset, batch_size=bs, shuffle=True, num_workers=5)
+dataloader_test = DataLoader(validset, batch_size=bs, shuffle=True, num_workers=5)
 
 net = models.resnet18(pretrained=True)
 num_ftrs = net.fc.in_features
 # Here the size of each output sample is set to 2.
 # Alternatively, it can be generalized to nn.Linear(num_ftrs, len(class_names)).
-net.fc = nn.Linear(num_ftrs, 100)
+net.fc = nn.Linear(num_ftrs, 2000)
 
 net = net.to(device)
 
@@ -45,7 +46,7 @@ net = net.to(device)
 
 
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(net.parameters(), lr=0.1)
+optimizer = optim.SGD(net.parameters(), lr=0.01, momentum =0.9, weight_decay = 0.00005)
 criterion = criterion.to(device)
 scheduler = StepLR(optimizer, step_size = 10, gamma=0.1)
 def voting(outputs):
@@ -111,7 +112,7 @@ with open(filename,'w') as csvfile:
             #running_acc += accuracy(predictions,labels)
             correct = (predictions == labels).sum().item()
             running_acc += (correct/(labels.size(0)))
-            if i%1 == 0:
+            if i%20 == 0:
                 #print("pred:", predictions)
                 csvwriter.writerow(['{}'.format(Identification),'{}'.format("Training"),'{}'.format(epoch),'{}'.format(training_loss/(i+1)),'{}'.format(running_acc/(i+1))])
                 print(f"Training phase, Epoch: {epoch}. Loss: {training_loss/(i+1)}. Accuracy: {running_acc/(i+1)}.")
@@ -148,10 +149,45 @@ with open(filename,'w') as csvfile:
                 #running_acc += accuracy(predictions,labels)
                 correct = (predictions == labels).sum().item()
                 running_acc += (correct/(labels.size(0)))
-            if i%1 == 0:
+            if i%10 == 0:
                 csvwriter.writerow(['{}'.format(Identification),'{}'.format("Validation"),'{}'.format(epoch),'{}'.format(valError/(i+1)),'{}'.format(running_acc/(i+1))])
                 print(f"Validation phase, Epoch: {epoch}. Loss: {valError/(i+1)}. Accuracy: {running_acc/(i+1)}.")
                 Identification += 1
+        scheduler.step()
+        testError = 0
+        running_acc = 0
+    for i, (inputs,labels) in enumerate(dataloader_test):
+        with torch.no_grad():
+            # get the inputs; data is a list of [inputs, labels]
+            inputs = inputs.view(-1, 3,64, 224, 224)
+            inputs = inputs.permute(0,2,1,3,4)
+            inputs = inputs.float()
+            inputs = inputs.to(device)
+            labels = torch.LongTensor(labels).to(device)
+
+            predictions = []
+            inner_loss = 0
+            for j in range((inputs.shape)[0]):
+                outputs = net(inputs[j])
+
+                frames_labels = ([labels[j]] * 64)
+                frames_labels = torch.tensor(frames_labels)
+                frames_labels = torch.LongTensor(frames_labels).to(device)
+                loss = criterion(outputs, (frames_labels))
+                predictions.append(voting(outputs))
+                inner_loss += loss.item()
+            predictions = torch.tensor(predictions)
+            predictions = predictions.to(device)
+            #running_acc += accuracy(predictions,labels)
+            correct = (predictions == labels).sum().item()
+            testError += (inner_loss/bs)
+            #running_acc += accuracy(predictions,labels)
+            correct = (predictions == labels).sum().item()
+            running_acc += (correct/(labels.size(0)))
+        if i%2 == 0:
+            csvwriter.writerow(['{}'.format(Identification),'{}'.format("Test"),'{}'.format(epoch),'{}'.format(testError/(i+1)),'{}'.format(running_acc/(i+1))])
+            print(f"Test phase, Epoch: {epoch}. Loss: {testError/(i+1)}. Accuracy: {running_acc/(i+1)}.")
+            Identification += 1
         scheduler.step()
     csvwriter.writerow(title)
 print('Finished Training')
